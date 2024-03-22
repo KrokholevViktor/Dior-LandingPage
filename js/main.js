bf461ae39628105de9c09095eb1444b0ae186b5e
Krokholev Viktor <xxxre.paprika@gmail.com>
2024-03-22 21:24:30 +0700

    slider about

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const swiper = new Swiper('.swiper', {
+const swiperTop = new Swiper('.top__swiper', {
     // Optional parameters
     direction: 'horizontal',
     effect: 'fade',
@@ -13,3 +13,14 @@ const swiper = new Swiper('.swiper', {
       prevEl: '.swiper-button-prev',
     },
   });
+
+const swiperAbout = new Swiper(".about__slider", {
+    slidesPerView: 4,
+    spaceBetween: 20,
+    freeMode: true,
+    // loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+});
